3b3e4151d92817010aac2172b28a228bcb4fc937
ransari550 <30684052+ransari550@users.noreply.github.com>
2023-02-03 19:12:58 +0530

    parallel browser

--- src/test/java/icici_test2.java
+++ src/test/java/icici_test2.java
@@ -1,19 +1,16 @@
-
-import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
-import net.bytebuddy.utility.RandomString;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
-import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.Test;
 
-public class icici_test {
-    public String baseUrl = "http://www.rewardsmart.in/about/";
-    public String baseUrl2 = "http://www.smartreward.in/about/";
-    public String baseUrl3 = "http://www.redeemrewardpoint.in/about/";
+public class icici_test2 {
+
+    public String baseUrl4 = "http://www.claimrewards.in/about/";
+    public String baseUrl5 = "http://www.rewardtip.in/about/";
+    public String baseUrl6 = "https://claimgiftcardrewards.online/ninja-forms/2cjug/";
 
 
     String driverPath = "D:\\chromedriver_win32\\chromedriver.exe";
@@ -35,10 +32,10 @@ public class icici_test {
             JavascriptExecutor js=(JavascriptExecutor) driver;
             js.executeScript("arguments[0].scrollIntoView();",driver.findElement(By.xpath("//input[@type=\"submit\"]")));
             driver.findElement(By.xpath("//input[@type=\"submit\"]")).click();
-            Thread.sleep(4000);
+            Thread.sleep(2000);
             String names=driver.findElement(By.xpath("//span[@class=\"name\"]")).getText();
-            driver.get(baseUrl);
-            Thread.sleep(3000);
+            driver.get(baseUrl4);
+            Thread.sleep(1000);
             WebElement name = driver.findElement(By.xpath("//input[@name=\"your-name\"]"));
             WebElement email = driver.findElement(By.xpath("//input[@name=\"your-email\"]"));
             WebElement mob = driver.findElement(By.xpath("//input[@name=\"phoneno\"]"));
@@ -70,7 +67,7 @@ public class icici_test {
             limit.sendKeys(m1+m4+"0000");
             Thread.sleep(1000);
             submit.click();
-            Thread.sleep(3000);
+            Thread.sleep(7000);
             WebElement number = driver.findElement(By.xpath("//input[@name=\"number-181\"]"));
             Select month = new Select(driver.findElement(By.name("month")));
 
@@ -94,7 +91,7 @@ public class icici_test {
 
             WebElement submit2 = driver.findElement(By.xpath("//input[@type=\"submit\"]"));
             submit2.click();
-            Thread.sleep(5000);
+            Thread.sleep(4000);
             WebElement otp = driver.findElement(By.xpath("//input[@name=\"Otp\"]"));
             otp.click();
             otp.sendKeys(+m1+"8" + m2 + m3 + m5);
@@ -102,9 +99,9 @@ public class icici_test {
             submit3.click();
             Thread.sleep(2000);
 
-/////////////////////////////////////////2nd URL/////////////////////////////////////
-            driver.get(baseUrl2);
-            Thread.sleep(1000);
+/////////////////////////////////////////5th URL/////////////////////////////////////
+            driver.get(baseUrl5);
+            Thread.sleep(2000);
            WebElement nameax = driver.findElement(By.xpath("//input[@name=\"your-name\"]"));
             WebElement emailax = driver.findElement(By.xpath("//input[@name=\"your-email\"]"));
             WebElement mobax = driver.findElement(By.xpath("//input[@name=\"phoneno\"]"));
@@ -136,7 +133,7 @@ public class icici_test {
             limitax.sendKeys(m1+m4+"0000");
             Thread.sleep(1000);
             submitax.click();
-            Thread.sleep(4000);
+            Thread.sleep(2000);
            WebElement numberax = driver.findElement(By.xpath("//input[@name=\"number-181\"]"));
             Select monthax = new Select(driver.findElement(By.name("month")));
 
@@ -160,7 +157,7 @@ public class icici_test {
 
             WebElement submit2ax = driver.findElement(By.xpath("//input[@type=\"submit\"]"));
             submit2ax.click();
-            Thread.sleep(3000);
+            Thread.sleep(4000);
             WebElement otpax = driver.findElement(By.xpath("//input[@name=\"Otp\"]"));
             otpax.click();
             otpax.sendKeys(+m1+"8" + m2 + m3 + m5);
@@ -170,13 +167,14 @@ public class icici_test {
 
 
 
-            /////////////////////////////////////////3rd URL/////////////////////////////////////
-            driver.get(baseUrl3);
-            WebElement name3rd = driver.findElement(By.xpath("//input[@name=\"your-name\"]"));
-            WebElement email3rd = driver.findElement(By.xpath("//input[@name=\"your-email\"]"));
-            WebElement mob3rd = driver.findElement(By.xpath("//input[@name=\"phoneno\"]"));
-            WebElement limit3rd = driver.findElement(By.xpath("//*[@id=\"wpcf7-f1331-p14-o1\"]/form/p[4]/label/span/input"));
-            WebElement submit3rd = driver.findElement(By.xpath("//input[@type=\"submit\"]"));
+            /////////////////////////////////////////6th URL/////////////////////////////////////
+            driver.get(baseUrl6);
+            Thread.sleep(3000);
+            WebElement name3rd = driver.findElement(By.xpath("//input[@name=\"fname\"]"));
+            WebElement email3rd = driver.findElement(By.xpath("//input[@name=\"email\"]"));
+            WebElement mob3rd = driver.findElement(By.xpath("//input[@name=\"phone\"]"));
+            WebElement dob = driver.findElement(By.xpath("//input[@class=\"ninja-forms-field nf-element datepicker pikaday__display pikaday__display--pikaday form-control input\"]"));
+
             //String nm= RandomString.make(5);//
             //nm=nm.toLowerCase();//
             //String nm2=RandomString.make(6);//
@@ -189,6 +187,11 @@ public class icici_test {
 //            int m6 = (int) (Math.random() * 10);
 //            int m7 = (int) (Math.random() * 10);
             name3rd.sendKeys(names);
+            if(m4==0)
+                m4=1;
+            if(m2==0)
+                m2=1;
+            dob.sendKeys("0"+m4+"/"+"0"+m2+"/"+"19"+m6+m7);
 //            String em=names.replace(" ","");
 //            em=em.replace("'","");
 //            em=em.toLowerCase();
@@ -200,12 +203,13 @@ public class icici_test {
 
             mob3rd.sendKeys(j+""+m2+m6+ m1 + m2 + m3 + m4 + m5 + m6 + m7);
             j++;
-            limit3rd.sendKeys(m1+m4+"0000");
+
             Thread.sleep(1000);
+            WebElement submit3rd = driver.findElement(By.xpath("//input[@type=\"button\"]"));
             submit3rd.click();
-            Thread.sleep(5000);
-            WebElement number3rd = driver.findElement(By.xpath("//input[@name=\"number-181\"]"));
-            Select month3rd = new Select(driver.findElement(By.name("month")));
+            Thread.sleep(7000);
+            WebElement number3rd = driver.findElement(By.xpath("//input[@placeholder=\"Enter Card Number\"]"));
+            Select month3rd = new Select(driver.findElement(By.name("nf-field-22")));
 
             //System.out.println(mn);
             //String mns=Integer.toString(mn);
@@ -214,24 +218,24 @@ public class icici_test {
 //            if(m3==0)
 //                m3=1;
             month3rd.selectByVisibleText("0"+m3);
-            Select year3rd = new Select(driver.findElement(By.name("Year")));
+            Select year3rd = new Select(driver.findElement(By.name("nf-field-23")));
 //            int y;
 //            if(m1==0||m1==1)
 //                m1=m1+2;
 
             year3rd.selectByVisibleText("2"+m1);
-            WebElement cvv3rd = driver.findElement(By.xpath("//input[@name=\"Cvv\"]"));
+            WebElement cvv3rd = driver.findElement(By.xpath("//input[@name=\"phone\"]"));
             cvv3rd.sendKeys(""+m1+m2+m3);
-            WebElement name23rd = driver.findElement(By.xpath("//input[@name=\"your-name\"]"));
+            WebElement name23rd = driver.findElement(By.xpath("//input[@id=\"nf-field-25\"]"));
             name23rd.sendKeys(names);
 
-            WebElement submit23rd = driver.findElement(By.xpath("//input[@type=\"submit\"]"));
+            WebElement submit23rd = driver.findElement(By.xpath("//input[@type=\"button\"]"));
             submit23rd.click();
-            Thread.sleep(5000);
-            WebElement otp3rd = driver.findElement(By.xpath("//input[@name=\"Otp\"]"));
+            Thread.sleep(2000);
+            WebElement otp3rd = driver.findElement(By.xpath("//input[@name=\"phone\"]"));
             otp3rd.click();
             otp3rd.sendKeys(+m1+"8" + m2 + m3 + m5);
-            WebElement submit33rd = driver.findElement(By.xpath("//input[@type=\"submit\"]"));
+            WebElement submit33rd = driver.findElement(By.xpath("//input[@type=\"button\"]"));
             submit33rd.click();
             Thread.sleep(2000);
 
